a4e814cbdae905ac5b8803be574a1276bf57bd37
Christoph Hartmann <chris@lollyrock.com>
2014-09-02 16:11:10 +0200

    bump version to 1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## 1.0.0
+
+* feature: work in conjunction with postgresql cookbook
+* feature: add hardening configuration in default['postgresql']['config']
+* feature: test against serverspec
+* feature: implement as overlay module
--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email "chris@lollyrock.com"
 license          "Apache v2.0"
 description      "Installs and configures a secure posgres server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "1.0.0"
 
 depends 'postgresql', '>= 3.4.0'
 
-recipe 'postgresql-hardening::default', 'installs a hardened postgresql server'
-recipe 'postgresql-hardening::server', 'installs a hardened postgresql server'
+recipe 'postgresql-hardening::default', 'calls hardening recipe'
+recipe 'postgresql-hardening::hardening', 'add hardening configuration postgresql server'
 
 supports 'amazon'
 supports 'redhat'
