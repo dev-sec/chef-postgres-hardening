134ec3cc018cebb264164a29c3c480ba971de54f
Patrick Münch <patrick.muench1111@gmail.com>
2016-07-05 13:04:15 +0200

    update metadata.rb to use postgresql cookbook version 4.0.6
    Signed-off-by: Patrick Münch <patrick.muench1111@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ description      'Installs and configures a secure posgres server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.0'
 
-depends 'postgresql', '>= 3.4.0'
+depends 'postgresql', '>= 4.0.6'
 
 recipe 'postgresql-hardening::default', 'calls hardening recipe'
 recipe 'postgresql-hardening::hardening', 'add hardening configuration postgresql server'
